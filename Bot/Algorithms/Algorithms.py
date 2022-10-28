8c761742c91e22664b8f3b2bb4a18f7508234f02
jake-hahne <113376744+jake-hahne@users.noreply.github.com>
2022-10-28 13:31:11 -0500

    format

--- Bot/Algorithms/Algorithms.py
+++ Bot/Algorithms/Algorithms.py
@@ -1,18 +1,20 @@
 import chess
 
+
 class AlgorithmBase():
     """Base class for chess"""
+
     def __init__(self, state=None):
         """Initializes a chess Board (if state is passed it will intialize based on state)"""
         self._board = chess.Board()
         if state is not None:
             for x in state.split(' '):
                 self._board.push(chess.Move.from_uci(x))
-    
+
     def moveUCI(self, e):
         """Adds a move using UCI (ex: e2e4 - StartEnd) format to the board"""
         self._board.push(chess.Move.from_uci(e))
-    
+
     def color(self):
         """Returns the color of the current player."""
         if self._board.turn:
