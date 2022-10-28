7edb4a0af95f324d188aca037e00c0ba44945af2
jake-hahne <113376744+jake-hahne@users.noreply.github.com>
2022-10-28 13:36:44 -0500

    placeholders for DumbBot Methods

--- Bot/Algorithms/DumbBot.py
+++ Bot/Algorithms/DumbBot.py
@@ -8,3 +8,17 @@ class DumbBot(Algorithms):
         if state is not None:
             for x in state.split(' '):
                 self._board.push(chess.Move.from_uci(x))
+
+    def moveUCI(self, e):
+        """Adds a move using UCI (ex: e2e4 - StartEnd) format to the board"""
+        self._board.push(chess.Move.from_uci(e))
+
+    def color(self):
+        """Returns the color of the current player."""
+        if self._board.turn:
+            return "White"
+        return "Black"
+
+    def isTurn(self, color):
+        """Returns True if color is for current player"""
+        return self.color().lower() == color.lower()
