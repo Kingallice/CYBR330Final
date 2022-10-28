487abe45d2399d08c8d603398782ff8ea9a64a6e
jake-hahne <113376744+jake-hahne@users.noreply.github.com>
2022-10-28 15:37:53 -0500

    fix

--- Bot/Algorithms/KillBot.py
+++ Bot/Algorithms/KillBot.py
@@ -1,21 +1,8 @@
-import Algorithms
+from Bot.Algorithms import Algorithms
 import chess
 
 
 class KillBot:
-    def __int__(self, state=None):
+    def __init__(self, state=None):
         self._base = Algorithms(state)
 
-    def moveUCI(self, e):
-        """Adds a move using UCI (ex: e2e4 - StartEnd) format to the board"""
-        self._board.push(chess.Move.from_uci(e))
-
-    def color(self):
-        """Returns the color of the current player."""
-        if self._board.turn:
-            return "White"
-        return "Black"
-
-    def isTurn(self, color):
-        """Returns True if color is for current player"""
-        return self.color().lower() == color.lower()
--- Bot/Algorithms/StockFish.py
+++ Bot/Algorithms/StockFish.py
@@ -1,4 +1,4 @@
-import Algorithms
+from Bot.Algorithms import Algorithms
 import chess
 
 
@@ -6,16 +6,3 @@ class StockFish:
     def __int__(self, state=None):
         self._base = Algorithms(state)
 
-    def moveUCI(self, e):
-        """Adds a move using UCI (ex: e2e4 - StartEnd) format to the board"""
-        self._board.push(chess.Move.from_uci(e))
-
-    def color(self):
-        """Returns the color of the current player."""
-        if self._board.turn:
-            return "White"
-        return "Black"
-
-    def isTurn(self, color):
-        """Returns True if color is for current player"""
-        return self.color().lower() == color.lower()
