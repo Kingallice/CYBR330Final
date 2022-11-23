9c49ae1842349d42578f830cdfbad7141255a287
Noah <noahmosel73@gmail.com>
2022-11-23 17:25:00 -0600

    Updates to StockFish algorithm

--- Bot/Logic/Stock.py
+++ Bot/Logic/Stock.py
@@ -5,9 +5,20 @@ import requests
 class StockFish(AlgorithmBase):
 
     def getMove(self):
-        return self.getStockFishSuggestions()[0]['uci']
+        try:
+            return self.getStockFishSuggestions()[0]
+        except:
+            return None
 
     def getStockFishSuggestions(self):
-        url = 'https://tablebase.lichess.ovh/standard?fen='+ self.getBoard().fen()
-        req = requests.get(url).json()['moves']
-        return req
+        url = 'https://lichess.org/api/cloud-eval?fen='+ self.getBoard().fen()
+        req = requests.get(url)
+        if req.status_code == 200:
+            return req.json()['pvs'][0]['moves'].split(' ')
+        else:
+            url = 'https://tablebase.lichess.ovh/standard?fen='+ self.getBoard().fen()
+            req = requests.get(url).json()['moves']
+            movesList = []
+            for move in req:
+                movesList.append(move['uci'])
+            return movesList
