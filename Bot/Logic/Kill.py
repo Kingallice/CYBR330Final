b78adc6010b19d04b8b401e982a382426d3b3873
Noah <noahmosel73@gmail.com>
2022-12-01 17:30:51 -0600

    Finished KillBot

--- Bot/Logic/Kill.py
+++ Bot/Logic/Kill.py
@@ -1,8 +1,15 @@
 from Bot.Logic.Base import AlgorithmBase
-import chess
-
+from Bot.Logic.Stock import StockFish
+import random
 
 class KillBot(AlgorithmBase):
 
-    pass
+    def getName():
+        return "Kill"
+    
+    def getMove(self):
+        moves = [move.uci() for move in self.getBoard().generate_legal_captures()]
+        if len(moves) > 0:
+            return random.choice(moves)
+        return StockFish(" ".join([move.uci() for  move in self.getBoard().move_stack])).getMove()
 
