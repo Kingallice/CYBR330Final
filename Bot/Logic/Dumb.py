2f049ba495d1d9d5e7fbfd41dfd55cd65d407ec7
g-olsen13 <olseng2@lopers.unk.edu>
2022-12-01 15:35:10 -0600

    small work done

--- Bot/Logic/Dumb.py
+++ Bot/Logic/Dumb.py
@@ -4,8 +4,19 @@ import chess
 
 class DumbBot(AlgorithmBase):
 
-    pass
+    def getMove(self):
+        # idk if i need this
+        legalmoves = []
+        legalcaptures = []
+        board = self.getBoard()
 
+    def wallBreaker(self):
 
+        board = self.getBoard()
+        if DumbBot.color() is "White":
+            # move the three pawns at c2,d2,e2 to c4,d4,e4
+            # this leaves the king open
+            if board.
 
-
+        # move pawns first then once pawns are gone then randomly move queen then move king straight forward
+        else:
