e569b0fc16b16649702aace6ff1de260f184f69a
Noah <noahmosel73@gmail.com>
2022-12-01 17:33:22 -0600

    main.py updates Implemented ResultUtil Setup the rest of the bots so they can be chosen in the GUI

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from GUI.main_gui import run_gui
 from Bot.Logic.Dumb import DumbBot
 from Bot.Logic.Kill import KillBot
 from Bot.Logic.Random import RandomBot
-from Bot.Logic.Copy import SmartBot
+from Bot.Logic.Copy import CopyBot
 from Bot.Logic.Stock import StockFish
 from Bot.Intialization.ChessBot import ChessBot
 from Bot.GameConnector import GameConnector
@@ -15,15 +15,15 @@ def bot_select():
     bot = ChessBot("lip_dQJI4YyJYwytH4emLoyq")
     
     selection = run_gui()
-    #if selection[0] == 'Dumb Bot':
-     #   
-    #if selection[0] == 'Random Bot':
-     #   bot = RandomBot()
-    #if selection[0] == 'Smart Bot':
-     #   bot = SmartBot()
-    #if selection[0] == 'Kill Bot':
-     #   bot = KillBot()
-    if selection[0] == 'Stock Fish':
+    if selection[0] == 'Dumb Bot':
+        GameConnector(bot, bot.getGames()[0]["gameId"],DumbBot)
+    elif selection[0] == 'Random Bot':
+        GameConnector(bot, bot.getGames()[0]["gameId"],RandomBot)
+    elif selection[0] == 'Copy Bot':
+        GameConnector(bot, bot.getGames()[0]["gameId"],CopyBot)
+    elif selection[0] == 'Kill Bot':
+        GameConnector(bot, bot.getGames()[0]["gameId"],KillBot)
+    elif selection[0] == 'Stock Fish':
         GameConnector(bot, bot.getGames()[0]["gameId"],StockFish)
     return bot
 
