c6b6f6bb994c39e2f34805e7791a37508e5bd563
Noah <noahmosel73@gmail.com>
2022-12-02 12:58:40 -0600

    ChallengeUtil

---
+++ Bot/ChallengeUtil.py
@@ -0,0 +1,32 @@
+from Bot.GameConnector import GameConnector
+import requests
+import threading
+LiChessAPI = "https://lichess.org/api/challenge/"
+
+class ChallengeUtil:
+
+    def sendChallenge(bot, opponent):
+        if "username" in opponent.keys():
+            if opponent["username"] == 'ai' and "level" in opponent.keys():
+                data = {'level': opponent['level']}
+            info = requests.post(LiChessAPI+opponent["username"], data=data, headers={"Authorization" : 'Bearer ' + bot.getKey()})
+            return(info.json()["id"])
+        return None
+    
+    def createChallenges(bot, opponent, num):
+        challenges = []
+        for i in range(num):
+            challenge = ChallengeUtil.sendChallenge(bot, opponent)
+            if challenge:
+                challenges.append(challenge)
+        return challenges
+
+    def testAlgorithm(bot, algo, opponentList, num):
+        for opponent in opponentList:
+            print(opponent)
+            challenges = ChallengeUtil.createChallenges(bot, opponent, num)
+            print(challenges)
+            for challenge in challenges:
+                newThread = threading.Thread(target=GameConnector, args=(bot, challenge, algo))
+                newThread.start()
+            
--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from Bot.Logic.Stock import StockFish
 from Bot.Intialization.ChessBot import ChessBot
 from Bot.GameConnector import GameConnector
 from Bot.ResultUtil import ResultUtil
+from Bot.ChallengeUtil import ChallengeUtil
 
 
 def bot_select():
     """Run GUI, initialize a bot type based on input from GUI"""
     ResultUtil.resetResultFile()
     bot = ChessBot("lip_dQJI4YyJYwytH4emLoyq")
-    
+
     selection = run_gui()
     if selection[0] == 'Dumb Bot':
         GameConnector(bot, bot.getGames()[0]["gameId"],DumbBot)
@@ -24,7 +25,7 @@ def bot_select():
     elif selection[0] == 'Kill Bot':
         GameConnector(bot, bot.getGames()[0]["gameId"],KillBot)
     elif selection[0] == 'Stock Fish':
-        GameConnector(bot, bot.getGames()[0]["gameId"],StockFish)
+        ChallengeUtil.testAlgorithm(bot, StockFish, [{"username":"ai","level":2},{"username":"ai","level":8}], 3)
     return bot
 
 bot_choice = bot_select()
