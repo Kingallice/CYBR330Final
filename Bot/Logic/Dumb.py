114aef0f98b730190c50b1fab37f9fd4733e574b
g-olsen13 <olseng2@lopers.unk.edu>
2022-12-02 12:18:13 -0600

    initial logic implemented for the first pawn moves

--- Bot/Logic/Dumb.py
+++ Bot/Logic/Dumb.py
@@ -1,3 +1,5 @@
+import random
+
 from Bot.Logic.Base import AlgorithmBase
 import chess
 
@@ -13,24 +15,32 @@ class DumbBot(AlgorithmBase):
         legal_Moves = board.generate_legal_moves()
 
         # need to move pawns at c7,d7,e7,f7 forwards 2 then moves them all forward one if applicable
-        # if chess.Board().piece_at(chess.C7):
-        #     chess.Move.from_uci('c7c5')
-        # elif chess.Board().piece_at(chess.D7):
-        #     chess.Move.from_uci('d7d5')
-        # elif chess.Board().piece_at(chess.E7):
-        #     chess.Move.from_uci('e7e5')
-        # elif chess.Board().piece_at(chess.F7):
-        #     chess.Move.from_uci('f7f5')
-        # elif chess.Board().piece_at(chess.C5):
-        #     chess.Move.from_uci('c5c4')
-        # elif chess.Board().piece_at(chess.D5):
-        #     chess.Move.from_uci('d5d4')
-        # elif chess.Board().piece_at(chess.E5):
-        #     chess.Move.from_uci('e5e4')
-        # elif chess.Board().piece_at(chess.F5):
-        #     chess.Move.from_uci('f5f4')
+        if chess.Board().piece_at(chess.C7):
+            chess.Move.from_uci('c7c5')
+        elif chess.Board().piece_at(chess.D7):
+            chess.Move.from_uci('d7d5')
+        elif chess.Board().piece_at(chess.E7):
+            chess.Move.from_uci('e7e5')
+        elif chess.Board().piece_at(chess.F7):
+            chess.Move.from_uci('f7f5')
+        elif chess.Board().piece_at(chess.C5):
+            chess.Move.from_uci('c5c4')
+        elif chess.Board().piece_at(chess.D5):
+            chess.Move.from_uci('d5d4')
+        elif chess.Board().piece_at(chess.E5):
+            chess.Move.from_uci('e5e4')
+        elif chess.Board().piece_at(chess.F5):
+            chess.Move.from_uci('f5f4')
+
+        # implementing random moves for when logic is not applicable for above
+        for moves in legal_Moves:
+            board = self.getBoard()
+            board.push_uci(moves.uci())
+            moves = random.choice(list(board.generate_legal_moves()))
+            return moves.uci()
 
         # attempting a optimized route
+
     def getName(self):
         return "Dumb Bot"
 
